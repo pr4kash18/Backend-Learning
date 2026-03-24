f3b2c509be5a2c5ec09aa431bb2b4e5865cac729
Chandra Prakash Choubisa <cpr4kash18@gmail.com>
2026-03-24 09:41:07 +0530

    updated new perfect code for database connection

---
+++ JDBC/basic_jdbc.java
@@ -0,0 +1,26 @@
+import java.sql.*;
+
+public class Main {
+    private static final String url = "jdbc:mysql://localhost:3306/learning";
+    private static final String username = "root";
+    private static final String password = "pr4kash18";
+
+    public static void main(String[] args) throws Exception{
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection con = DriverManager.getConnection(url, username, password);
+
+
+        PreparedStatement ps = con.prepareStatement("INSERT INTO register VALUES ('prakash', 'prakash123@gmail.com', 'prakash123', 'male', 'udaipur')");
+        int i = ps.executeUpdate();
+        if (i>0){
+            System.out.println("Successfully Executed! ");
+        } else {
+            System.out.println("Failed");
+        }
+
+        }
+
+}
+
