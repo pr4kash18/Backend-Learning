6b07df4eab04efeb90595c5fdace9aa47f7938a6
Chandra Prakash Choubisa <cpr4kash18@gmail.com>
2026-03-24 21:13:13 +0530

    Update database connection

--- JDBC/delete_db.java
+++ JDBC/delete_db.java
@@ -6,7 +6,7 @@ public class DeleteDemo {
     public static void main(String[] args) throws Exception{
         String name2="bhavesh";
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/learning", "root", "pr4kash18");
+        Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/learning", "root", "root");
         PreparedStatement ps = connect.prepareStatement("DELETE FROM register WHERE name=?");
         ps.setString(1, name2);
         int i = ps.executeUpdate();
